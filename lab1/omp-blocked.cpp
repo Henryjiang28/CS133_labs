23faa0f7ef155815ea14d826c578d1e2d00491a7
Henry Jiang <henryjiang.xinle@gmail.com>
2025-04-12 21:26:52 -0700

    1.3s and 0.98s

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -1,10 +1,42 @@
-// Header inclusions, if any...
+#include <cmath>
+#include <cstring>
+
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <omp.h>
 
 #include "lib/gemm.h"
+#define BLOCK_I 64
+#define BLOCK_J 64
+#define BLOCK_K 64
 
 // Using declarations, if any...
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
-  // Your code goes here...
-}
+
+
+  for (int i = 0; i < kI; ++i) {
+      std::memset(c[i], 0, sizeof(float) * kJ);
+    }
+
+    #pragma omp parallel for collapse(2)
+    for (int ii = 0; ii < kI; ii += BLOCK_I) {
+      for (int jj = 0; jj < kJ; jj += BLOCK_J) {
+        for (int kk= 0; kk < kK; kk += BLOCK_K) {
+            for (int i = ii; i < std::min(ii + BLOCK_I, kI); ++i) {
+              for (int k = kk; k < std::min(kk + BLOCK_J, kK); ++k) {
+                for (int j = jj; j < std::min(jj + BLOCK_K, kJ); ++j) {
+                  c[i][j] += a[i][k] * b[k][j];
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+
--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -1,4 +1,10 @@
+#include <cmath>
+#include <cstring>
+
+#include <chrono>
 #include <iostream>
+#include <random>
+#include <vector>
 #include <omp.h>
 
 #include "lib/gemm.h"
@@ -7,5 +13,35 @@
 
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
-  // Your code goes here...
-}
+    for (int i = 0; i < kI; ++i) {
+      std::memset(c[i], 0, sizeof(float) * kJ);
+    }
+
+    int i, j, k;
+    #pragma omp parallel for collapse(2)
+    for (i = 0; i < kI; ++i) {
+      for (k = 0; k < kK; ++k) {
+        for (j = 0; j < kJ; ++j) {
+          c[i][j] += a[i][k] * b[k][j];
+        }
+      }
+    }
+  }
+
+  // void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
+  //                 float c[kI][kJ]) {
+  //   for (int i = 0; i < kI; ++i) {
+  //     std::memset(c[i], 0, sizeof(float) * kJ);
+  //   }
+
+  
+  //   for (int i = 0; i < kI; ++i) {
+  //       for (int j = 0; j < kJ; ++j) {
+  //         for (int k = 0; k < kK; ++k) {
+  //           c[i][j] += a[i][k] * b[k][j];
+  //       }
+  //     }
+  //   }
+  // }
+
+
