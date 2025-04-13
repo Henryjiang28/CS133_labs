88935b69dd95749783cba112bf8d43b34213d0bd
Henry Jiang <henryjiang.xinle@gmail.com>
2025-04-12 17:34:18 -0700

    lab1: first version

---
+++ lab1/omp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <omp.h>
+
+#include "lib/gemm.h"
+
+// Using declarations, if any...
+
+void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
+                  float c[kI][kJ]) {
+  // Your code goes here...
+}
